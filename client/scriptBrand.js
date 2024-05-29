cf4763ab84b15c042a51add1b0ba2073192434ac
kingwilldabeast <willmur@live.com>
2024-05-29 15:01:34 -0500

    updated three client files

--- client/scriptBrand.js
+++ client/scriptBrand.js
@@ -1,25 +1,11 @@
 let button = document.querySelector("#searchButton")
 
 let container = document.querySelector("#container")
-
-// let card = document.querySelector(".card")
-// let movieName = document.querySelector("#brand-name")
-// let movieImage = document.querySelector('#brand-logo')
-
-// let value1 = document.querySelector("#value1")
-// let value2 = document.querySelector("#value2")
-// let value3 = document.querySelector("#value3")
-// let value4 = document.querySelector("#plot-content")
-// let actorElemList = document.querySelector('#actor-list')
-// let reviewList = document.querySelector('#review-list')
-// let actorButtons = document.querySelectorAll('.actor-button')
-// let actorInfo = document.querySelector('#actor-info')
-// let actorName = document.querySelector('#actor-name')
-// let actorHeadshot = document.querySelector('#actor-headshot')
-// let closeButton = document.querySelector('#close')
+let cards = document.querySelectorAll(".card")
 
 let brandNameArray = []
 let brandLogoArray = []
+let brandIDArray = []
 
 // let input = document.querySelector("#inputBar").value
 // if (input == ``) { return }
@@ -30,19 +16,20 @@ document.addEventListener('DOMContentLoaded', async ()=> {
   try {
     // let response = await axios.get(`http://localhost:3001/brands/${input}`);
     let response = await axios.get(`http://localhost:3001/brands`);
-    console.log(response.data); // Log the data received from the API
+    // console.log(response.data); // Log the data received from the API
     
     brandArray = response.data //assuming it's an array
-    console.log(brandArray)
+    // console.log(brandArray)
         
     for (const brandObject of brandArray) {
 
         const newCard = document.createElement('div');
         newCard.classList.add('card');
+        newCard.setAttribute('id',brandObject._id)
 
         const newImage = document.createElement('img');
         newImage.classList.add('brand-logo')
-        console.log(brandObject.logo_img)
+        // console.log(brandObject.logo_img)
         newImage.setAttribute('src',brandObject.logo_img)
 
         const newName = document.createElement('h2');
@@ -51,97 +38,40 @@ document.addEventListener('DOMContentLoaded', async ()=> {
         
         newCard.appendChild(newImage)
         newCard.appendChild(newName)
-        container.appendChild(newCard);
+
+        const newBicycleLink = document.createElement('a');
+        // newBicycleLink.href = 'parkerBicycle.html';
+        // newBicycleLink.target = '_blank'; 
+        newBicycleLink.appendChild(newCard);
+        
+        container.appendChild(newBicycleLink);
 
         brandNameArray.push(brandObject.name)
         brandLogoArray.push(brandObject.logo_img)
-
+        brandIDArray.push(brandObject._id)
+        
     }
-
-
-    // movieName.innerText = singleMovie.title
-    // movieImage.setAttribute('src',singleMovie.poster_img)
-    // value1.innerText = singleMovie.releaseYear
-    // value2.innerText = singleMovie.rating
-    // value3.innerText = singleMovie.runtimeMinutes
-    // value4.innerText = singleMovie.description
     
-    // let actorsIDArray = singleMovie.actors
-
-    // const fetchActorDetails = async (actorID) => {
-    //   try {
-    //     // console.log(actorID);
-    //     let singleActor = await axios.get(`http://localhost:3001/actors/${actorID}`);
-    //     const newElement = document.createElement('div');
-    //     newElement.classList.add('info','actor-button');
-    //     // newElement.innerHTML = `<a href = http://localhost:3001/actors/${actorID}>${singleActor.data.name}</a>`
-    //     newElement.innerText = singleActor.data.name
-    //     actorElemList.appendChild(newElement);
-    //     actorArray.push(singleActor.data.name)
-    //     actorImageArray.push(singleActor.data.headshot_img)
-    //   } catch (error) {
-    //     console.error(`Error fetching actor with ID ${actorID}:`, error);
-    //   }
-    // };
+    // console.log(brandLogoArray)
+    // console.log(brandIDArray)
+    cards = document.querySelectorAll(".card")
+    // console.log(cards)
     
-    // const loadActors = async () => {
-    //   actorElemList.innerHTML = ''
-    //   for (const actorID of actorsIDArray) {
-    //     await fetchActorDetails(actorID);
-    //   }
-    //   console.log(actorArray)
-    //   console.log(actorImageArray)
-    //   actorButtons = document.querySelectorAll('.actor-button')
-    //   actorButtons.forEach(function(actorButton) {
-    //     actorButton.addEventListener('click', function() {    
-    //       actorInfo.style.zIndex = 2 
-    //       const position = actorArray.indexOf(actorButton.innerText)
-    //       actorName.innerText = actorArray[position]
-    //       actorHeadshot.setAttribute('src', actorImageArray[position])
-    //       // console.log('hi')
-    //     });
-    // });
-    //   // console.log(actorButtons)
-
-    // };
     
-    // loadActors();
-
-    /////Revies
-    // let reviewsIDArray = singleMovie.reviews
-    // console.log(`THE REVIEW array IS ${reviewsIDArray}`);
-
-
-    // const fetchReviewDetails = async (reviewID) => {
-    //   try {
-    //     // console.log(`THE REVIEW ID IS ${reviewID}`);
-    //     let singleReview = await axios.get(`http://localhost:3001/reviews/${reviewID}`);
-    //     const newElement = document.createElement('div');
-    //     newElement.classList.add('info');
-    //     newElement.innerHTML = `<a href = http://localhost:3001/reviews/${reviewID}>${singleReview.data.comment}</a>`
-    //     reviewList.appendChild(newElement);
-    //   } catch (error) {
-    //     console.error(`Error fetching review with ID ${reviewID}:`, error);
-    //   }
-    // };
+    cards.forEach(function(card) {
+      card.addEventListener('click', function() {    
+        // idCopy = card.querySelector('.brand-name').innerText
+        idCopy = card.getAttribute('id')
+        localStorage.setItem('brandID', idCopy);
+        console.log(idCopy)
+        window.open('parkerBicycle.html', '_blank');
+      });
     
-    // const loadReviews = async () => {
-    //   reviewList.innerHTML = ''
-    //   for (const reviewID of reviewsIDArray) {
-    //     await fetchReviewDetails(reviewID);
-    //   }
-    // };
     
-    // loadReviews();
-    //reviews...
-
-  } catch (error) {
+    });
+    
+    
+} catch (error) {
     console.error('Error fetching data:', error);  
-  }
-  })
-
-//   closeButton.addEventListener('click', () => {
-//       actorInfo.style.zIndex = -1
-//   })
-
-
+}
+})
