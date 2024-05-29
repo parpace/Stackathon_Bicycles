5b42934027bc2290ac5b267b0a57abfbd685486d
sebcuellar6 <seabasscuellarh@gmail.com>
2024-05-29 10:37:34 -0500

    added logos to brands

--- controllers/BellController.js
+++ controllers/BellController.js
@@ -8,3 +8,40 @@ const getAllBells = async (req, res) => {
         return res.status(500).send(error.message);
     }
 }
+
+const getBellById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const singleObject = await Bell.findById(id)
+        if (singleObject) {
+            return res.json(singleObject)
+        }
+        return res.status(404).send(`that Bicycle doesn't exist`)
+    } catch (error) {
+        if (error.name === 'CastError' && error.kind === 'ObjectId') {
+            return res.status(404).send(`That Bicycle doesn't exist`)
+        }
+        return res.status(500).send(error.message);
+    }
+}
+
+const createBell = async (req, res) => {
+    try {
+        const newObject = await new Bell(req.body)
+        await newObject.save()
+        return res.status(201).json({
+            newObject,
+        });
+    } catch (error) {
+        // if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        //     return res.status(404).send(`That Bicycle doesn't exist`)
+        // }
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+module.exports = {
+    getAllBells,
+    getBellById,
+    createBell
+}
